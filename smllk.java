5681bbe4d60eb9874129cfb81b7b27dc41768849
Brandon Chao <bchao1@stuy.edu>
2019-03-11 14:52:15 -0400

    checking for partition

--- smllk.java
+++ smllk.java
@@ -2,8 +2,19 @@ import java.util.Random;
 
 public class smllk{
 
+  public static void main(String[] args) {
+    int[] d = {42,2,5,23,56,12};
+    int a = partition(d,0,d.length-1);
+    System.out.println(a);
+    System.out.println(check(d,a));
+    String as = "";
+    for (int x = 0; x < d.length; x++) {
+      as += d[x] + " ";
+    }
+    System.out.println(d);
+  }
 
-  public int partition (int [] data, int start, int end) {
+  public static int partition (int [] data, int start, int end) {
 
     Random r = new Random();
     int starter = start;
@@ -11,6 +22,8 @@ public class smllk{
     int pivot = r.nextInt(data.length);
     int compare;
     int storage;
+
+    System.out.println(pivot);
 
     compare = data[pivot];
     data[pivot] = data[starter];
@@ -39,8 +52,24 @@ public class smllk{
         data[ender] = data[x];
         data[x] = storage;
         ender--;
+        starter--;
       }
     }
     return -1;
+  }
+
+  public static boolean check(int data[], int index) {
+    for (int x = 0 ; x < index; x++) {
+      if (data[x] > data[index]) {
+        return false;
+      }
+    }
+    for (int y = data.length -1; y > index; y--) {
+      System.out.println("y + " + y);
+      if (data[y] < data[index]) {
+        return false;
+      }
+    }
+    return true;
   }
 }
