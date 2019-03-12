725688b06b40b9c59f262964c5abc753421d91c8
Brandon Chao <bchao1@stuy.edu>
2019-03-12 14:52:12 -0400

    partition

--- smllk.java
+++ smllk.java
@@ -5,13 +5,13 @@ public class smllk{
   public static void main(String[] args) {
     int[] d = {42,2,5,23,56,12};
     int a = partition(d,0,d.length-1);
-    System.out.println(a);
+    System.out.println("a + " + a);
     System.out.println(check(d,a));
     String as = "";
     for (int x = 0; x < d.length; x++) {
       as += d[x] + " ";
     }
-    System.out.println(d);
+    System.out.println(as);
   }
 
   public static int partition (int [] data, int start, int end) {
@@ -19,41 +19,39 @@ public class smllk{
     Random r = new Random();
     int starter = start;
     int ender = end;
-    int pivot = r.nextInt(data.length);
-    int compare;
+    int pivot = r.nextInt(data.length-1);
     int storage;
 
     System.out.println(pivot);
 
-    compare = data[pivot];
+    storage = data[pivot];
     data[pivot] = data[starter];
-    data[starter] = compare;
+    data[starter] = storage;
 
-    starter++;
-
-    for (int x = starter; starter < data.length ; starter++) {
+    for (int x = starter+1; x < data.length ; x++) {
       if (x == ender) {
-        if (data[x] <= compare) {
+        if (data[x] <= data[starter]) {
           storage = data[x];
-          data[x] = data[start];
-          data[start] = data[x];
+          data[x] = data[starter];
+          data[starter] = storage;
           return x;
         }
         else {
           storage = data[x-1];
-          data[x-1] = data[start];
-          data[start] = data[x-1];
+          data[x-1] = data[starter];
+          data[starter] = storage;
           return x-1;
         }
 
       }
-      if (data[x] > compare) {
+      if (data[x] > data[starter]) {
         storage = data[ender];
         data[ender] = data[x];
         data[x] = storage;
         ender--;
-        starter--;
+        x--;
       }
+      
     }
     return -1;
   }
@@ -66,6 +64,7 @@ public class smllk{
     }
     for (int y = data.length -1; y > index; y--) {
       System.out.println("y + " + y);
+      System.out.println("index + " + index);
       if (data[y] < data[index]) {
         return false;
       }
